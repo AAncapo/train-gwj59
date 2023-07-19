bb07af60e6cdca867b426415ada53ba2576f28b7
AAncapo <raidellg9511@gmail.com>
2023-07-19 02:43:13 -0500

    added more arrow buttons
    # created gui theme

--- main/main_menu.gd
+++ main/main_menu.gd
@@ -5,12 +5,13 @@ func toggle_display(display):
 	get_tree().paused = display
 	visible = display
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if display else Input.MOUSE_MODE_CAPTURED
-	
-	if !display:
-		$"%Intro".hide()
+	$"%Board".visible = !display
+	$VBoxContainer/retry.disabled = !Global.game_started
+
 
 func _on_play_pressed():
 	toggle_display(!visible)
+	Global.game_started = true
 
 func _on_options_pressed():
 	$"%options_menu".show()
@@ -18,3 +19,7 @@ func _on_options_pressed():
 func _on_exit_pressed():
 	get_tree().quit()
 
+
+func _on_retry_pressed():
+	get_tree().reload_current_scene()
+	$VBoxContainer/play.emit_signal("pressed") ##TODO: NOT WORKING
--- main/spawner.gd
+++ main/spawner.gd
@@ -1,11 +1,15 @@
-extends HBoxContainer
+extends Control
 class_name Spawner
 
+onready var spwn_pos = $HBoxContainer.get_children()
 var arrow_tscn = preload("res://rythm_system/arrow.tscn")
 var arrows = ['up','down','left','right']
+
 
 func spawn_arrow():
 	var new_arrow = arrow_tscn.instance()
 	new_arrow.a_name = arrows[0]
-	$Control/Spawner.add_child(new_arrow)
+	for spwn_node in spwn_pos:
+		if spwn_node.name == new_arrow.a_name:
+			spwn_node.add_child(new_arrow)
 	arrows.shuffle()
